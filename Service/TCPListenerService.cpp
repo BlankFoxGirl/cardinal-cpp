48deea5b947ad7c8821cac010a3c43ccfd519c9a
Sarah Jabado <sarmth@icloud.com>
2023-05-10 08:15:17 +0800

    Refactor TCPListenerService and correct spelling typo. Split interface from implementation.

--- Service/TCPListenerService.cpp
+++ Service/TCPListenerService.cpp
@@ -22,36 +22,41 @@ using namespace std;
 
 using namespace Cardinal::Service;
 
-typedef void * (*THREADFUNCPTR)(void *);
-pthread_t thread_tcp [100], thread_file [10];
+typedef void *(*THREADFUNCPTR)(void *);
+pthread_t thread_tcp[100], thread_file[10];
 int threadno_tcp = 0;
 
 /* message type that will be sent by the client through TCP connection */
-struct msg {
-    char filename [100];
-    char file [2048];
+struct msg
+{
+    char filename[100];
+    char file[2048];
 };
 
 /* Structure to hold the necessary parameters to pass into the threaded function */
-struct req {
+struct req
+{
     int des;
     socklen_t addlen;
     sockaddr_in clientaddr;
 };
 
-TCPListernerService::TCPListernerService() {
+TCPListenerService::TCPListenerService()
+{
     // Default values.
     this->Port = stoi("7777");
     this->Address = inet_pton(AF_INET, "0.0.0.0", NULL);
 }
 
-TCPListernerService::TCPListernerService(string port) {
+TCPListenerService::TCPListenerService(string port)
+{
     // Specified Port!
     this->Port = stoi(port);
     this->Address = inet_pton(AF_INET, "0.0.0.0", NULL);
 }
 
-TCPListernerService::TCPListernerService(string port, char* address) {
+TCPListenerService::TCPListenerService(string port, char *address)
+{
     // Specified Port!
     this->Port = stoi(port);
 
@@ -59,66 +64,113 @@ TCPListernerService::TCPListernerService(string port, char* address) {
     this->Address = inet_pton(AF_INET, address, NULL);
 }
 
-void TCPListernerService::Start() {
-    sockaddr_in serverAddress; // server address
+void TCPListenerService::Start()
+{
     sockaddr_in clientaddr; // client address
-
+    sockaddr_in serverAddress = ConfigureServerAddress();
     socklen_t addrlen = sizeof(clientaddr);
 
-    /* Create socket */
-    if ((TCPListernerService::mistfd = socket (AF_INET, SOCK_STREAM, 0)) == -1) {
-        throw Cardinal::Exception::TCPSocketCreationException();
-    }
-    memset ((sockaddr*)&serverAddress, 0, sizeof (serverAddress));
-    serverAddress.sin_family = AF_INET;   // IPv4 address family
-    serverAddress.sin_addr.s_addr = htons (this->Address);  // Give the local machine address
-    serverAddress.sin_port = htons (this->Port); // Port at which server listens to the requests
+    CreateSocketStream();
+    StoreServerAddressInMemory(serverAddress);
+    BindServerAddressAndPort(serverAddress);
+    ListenForConnections();
+    SetSignalHandlers();
 
-    /* Bind the IP address and the port number to craete the socket */
-    if (::bind (TCPListernerService::mistfd, (sockaddr*)&serverAddress, sizeof (serverAddress)) == -1) {
-        throw Cardinal::Exception::TCPSocketBindException();
-    }
-
-    if ((listen (TCPListernerService::mistfd, 5)) != 0) {
-        throw Cardinal::Exception::TCPSocketListenException();
-    }
-
-    signal(SIGINT, TCPListernerService::sig_handler);
-    signal(SIGTSTP, TCPListernerService::sig_handler);
-
-    while (1) {
-
-        int connfd;
-        if ((connfd = accept(TCPListernerService::mistfd,(sockaddr*)&clientaddr, &addrlen)) < 0) {
-            std::cout << "\n\t Client connection declined..." << '\n';
-        } else {
-            std::cout << "\n\t Client connection accepted..." << '\n';
-        }
-
-        /* Filling the parameter values of the threaded function */
-        req *r = new req;  // allocate memory
-        bzero (r, sizeof (req));  // Clear memory
-        r->addlen = addrlen;
-        r->clientaddr = clientaddr;
-        r->des = connfd;
-
-        pthread_create (&thread_tcp [threadno_tcp++], NULL, (THREADFUNCPTR) &TCPListernerService::Connection, (void*)r);
-
-        if (threadno_tcp == 100) {
-            threadno_tcp = 0;
-        }
-   }
+    ServerLoop(clientaddr, addrlen);
 }
 
-void* TCPListernerService::LocalConnection(void* args) {
-    req sock = *((req*)args);
+void TCPListenerService::ListenForConnections()
+{
+    if ((listen(TCPListenerService::mistfd, 5)) != 0)
+    {
+        throw Cardinal::Exception::TCPSocketListenException();
+    }
+}
+
+void TCPListenerService::BindServerAddressAndPort(sockaddr_in serverAddress)
+{
+    if (::bind(TCPListenerService::mistfd, (sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
+    {
+        throw Cardinal::Exception::TCPSocketBindException();
+    }
+}
+
+void TCPListenerService::StoreServerAddressInMemory(sockaddr_in serverAddress)
+{
+    memset((sockaddr *)&serverAddress, 0, sizeof(serverAddress));
+}
+
+void TCPListenerService::SetSignalHandlers()
+{
+    signal(SIGINT, TCPListenerService::sig_handler);
+    signal(SIGTSTP, TCPListenerService::sig_handler);
+}
+
+void TCPListenerService::ServerLoop(sockaddr_in &clientaddr, socklen_t &addrlen)
+{
+    while (1)
+    {
+        ServerLoop(clientaddr, addrlen);
+    }
+}
+
+void TCPListenerService::ServerLoopInnerContent(sockaddr_in &clientaddr, socklen_t &addrlen)
+{
+    int connfd;
+    if ((connfd = accept(TCPListenerService::mistfd, (sockaddr *)&clientaddr, &addrlen)) < 0)
+    {
+        std::cout << "\n\t Client connection declined..." << '\n';
+    }
+    else
+    {
+        std::cout << "\n\t Client connection accepted..." << '\n';
+    }
+
+    /* Filling the parameter values of the threaded function */
+    req *r = new req;      // allocate memory
+    bzero(r, sizeof(req)); // Clear memory
+    r->addlen = addrlen;
+    r->clientaddr = clientaddr;
+    r->des = connfd;
+
+    pthread_create(&thread_tcp[threadno_tcp++], NULL, (THREADFUNCPTR)&TCPListenerService::Connection, (void *)r);
+
+    if (threadno_tcp == 100)
+    {
+        threadno_tcp = 0;
+    }
+}
+
+void *TCPListenerService::LocalConnection(void *args)
+{
+    req sock = *((req *)args);
     msg buffer;
 
-    while (1) {
+    while (1)
+    {
         /* Read the packet sent by the established client */
-        read (sock.des, &buffer, sizeof(buffer));
+        read(sock.des, &buffer, sizeof(buffer));
 
         std::cout << "\n\t File name: " << buffer.filename;
-        std::cout << "\n\n" << buffer.file << '\n';
+        std::cout << "\n\n"
+                  << buffer.file << '\n';
     }
+}
+
+void TCPListenerService::CreateSocketStream()
+{
+    if ((TCPListenerService::mistfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        throw Cardinal::Exception::TCPSocketCreationException();
+    }
+}
+
+sockaddr_in TCPListenerService::ConfigureServerAddress()
+{
+    sockaddr_in serverAddress;                            // server address
+    serverAddress.sin_family = AF_INET;                   // IPv4 address family
+    serverAddress.sin_addr.s_addr = htons(this->Address); // Give the local machine address
+    serverAddress.sin_port = htons(this->Port);           // Port at which server listens to the requests
+
+    return serverAddress;
 }
--- Service/TCPListenerService.h
+++ Service/TCPListenerService.h
@@ -1,31 +1,59 @@
 #include <string>
 #include <unistd.h>
 #include <signal.h>
+#include <iostream>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <signal.h>
+#include <arpa/inet.h>
 using namespace std;
 
-namespace Cardinal::Service {
-    class TCPListernerService {
-        TCPListernerService();
-        TCPListernerService(string port);
-        TCPListernerService(string port, char* address);
-        void* LocalConnection(void* args);
+namespace Cardinal::Service
+{
+    class TCPListenerService
+    {
+        TCPListenerService();
+        TCPListenerService(string port);
+        TCPListenerService(string port, char *address);
+        void *LocalConnection(void *args);
         void Start();
-        static void* Connection(void* args) {
-            TCPListernerService t = TCPListernerService();
+        static void *Connection(void *args)
+        {
+            TCPListenerService t = TCPListenerService();
             t.LocalConnection(args);
         }
-        static void sig_handler(int signo) {
+        static void sig_handler(int signo)
+        {
             /* signal handler */
-            if (signo == SIGINT) {
-                close (TCPListernerService::mistfd);
-                exit (1);
+            if (signo == SIGINT)
+            {
+                close(TCPListenerService::mistfd);
+                exit(1);
             }
         }
 
-        private:
-            u_int32_t Port;
-            u_int32_t Address;
-            static int mistfd;
+    private:
+        u_int32_t Port;
+        u_int32_t Address;
+        static int mistfd;
+        void CreateSocketStream();
+        sockaddr_in ConfigureServerAddress();
+        void ServerLoop(sockaddr_in &clientaddr, socklen_t &addrlen);
+        void ServerLoopInnerContent(sockaddr_in &clientaddr, socklen_t &addrlen);
+        void SetSignalHandlers();
+        void StoreServerAddressInMemory(sockaddr_in serverAddress);
+        void BindServerAddressAndPort(sockaddr_in serverAddress);
+        void ListenForConnections();
     };
 }
-int Cardinal::Service::TCPListernerService::mistfd = 0;
+int Cardinal::Service::TCPListenerService::mistfd = 0;
