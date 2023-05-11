17765da01cad709f86577d5374e63c3e4b9ce5b8
Sarah Jabado <sarmth@icloud.com>
2023-05-11 09:26:29 +0800

    Implement MessageService in EventMap

--- Event/EventMap.h
+++ Event/EventMap.h
@@ -1,8 +1,9 @@
 #include <string>
 #include <map>
 #include <iostream>
-#include "../Exception/InvalidOrMissingEvent.h"
+#include "../Exception/Exceptions.h"
 #include "Events.h"
+#include "../Service/MessageService.h"
 #ifndef EventMap_H
 #define EventMap_H
 using namespace std;
@@ -11,15 +12,14 @@ namespace Cardinal::Event {
     typedef map<string, Cardinal::Event::AbstractEvent*> eventObject;
     class EventMap {
         public:
-            static void Invoke (string eventName, string payload) {
-                auto receivedEventObject = events.at(eventName);
+            static void Invoke (string channel, string message) {
                 try {
-                    receivedEventObject->invoke(payload);
-                } catch (std::exception& e) {
-                    throw Cardinal::Exception::InvalidOrMissingEvent();
+                    EventMap::RetrieveAndInvokeEventObjectFromMessage(message);
+                } catch (Cardinal::Exception::InvalidOrMissingEvent& e) {
+                    cout << "Invalid or missing event " << message << " Channel: " << channel << endl;
+                } catch (Cardinal::Exception::InvalidMessage& e) {
+                    cout << "Invalid message " << message << " Channel: " << channel << endl;
                 }
-                receivedEventObject->invoke(payload);
-                cout << "Ran:" << eventName << endl;
             }
 
             static void Register (string eventName, Cardinal::Event::AbstractEvent *eventObject) {
@@ -27,6 +27,34 @@ namespace Cardinal::Event {
             }
 
             static eventObject events;
+        private:
+            static std::__1::tuple<std::__1::string, std::__1::string> RetrieveEventFromMessage (string message) {
+                Cardinal::Service::MessageService messageService = Cardinal::Service::MessageService(message);
+                return messageService.Decode();
+            }
+
+            static void RetrieveAndInvokeEventObjectFromMessage(string Message) {
+                auto [eventName, payload] = EventMap::RetrieveEventFromMessage(Message);
+                EventMap::RetrieveAndInvokeEventObject(eventName, payload);
+            }
+
+            static void RetrieveAndInvokeEventObject(string EventName, string Payload) {
+                try {
+                    auto receivedEventObject = events.at(EventName);
+                    receivedEventObject->invoke(Payload);
+                } catch (std::exception& e) {
+                    throw Cardinal::Exception::InvalidOrMissingEvent();
+                }
+            }
+
+            static Cardinal::Event::AbstractEvent* GetEventObject (string eventName) {
+                try {
+                    auto receivedEventObject = events.at(eventName);
+                    return receivedEventObject;
+                } catch (std::exception& e) {
+                    throw Cardinal::Exception::InvalidOrMissingEvent();
+                }
+            }
     };
 }
 
