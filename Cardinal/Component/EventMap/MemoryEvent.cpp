3deb60e6c175cb91130507d16701cb6d3aaed0b7
Sarah Jabado <sarmth@icloud.com>
2023-06-01 14:06:59 +0800

    Optimised message payloads with message references.

---
+++ Cardinal/Component/EventMap/MemoryEvent.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include "Cardinal/Service/Services.hpp"
+#include "Cardinal/Entity/EventEntity.hpp"
+#include "Cardinal/Entity/Message.hpp"
+#include "Cardinal/Component/EventMap/MemoryEvent.hpp"
+
+using namespace Cardinal::Component::EventMap;
+
+void MemoryEvent::SendOptimisedMessage(
+    std::string connectionUUID,
+    Cardinal::Entity::Message& message,
+    Service::MemoryServiceInterface& memoryService,
+    Service::MessageServiceInterface& messageService
+) {
+    auto eventEntity = Cardinal::Entity::EventEntity(
+            message.getKey(),
+            message.getPayload(),
+            connectionUUID,
+            "0"
+        );
+
+    memoryService.WriteAllHash(eventEntity.GetQueue(message.getUUID()), eventEntity.Serialize());
+
+    message.setPayload(message.getUUID()); // Overwite payload with message UUID then publish to workers.
+
+    messageService.Dispatch(message);
+}
---
+++ Cardinal/Component/EventMap/MemoryEvent.hpp
@@ -0,0 +1,23 @@
+#ifndef CARDINAL_COMPONENT_EVENTMAP_MEMORYEVENT_HPP
+#define CARDINAL_COMPONENT_EVENTMAP_MEMORYEVENT_HPP
+
+#include <string>
+#include "Cardinal/Entity/Message.hpp"
+#include "Cardinal/Service/Services.hpp"
+#include "Cardinal/Entity/EventEntity.hpp"
+
+namespace Cardinal::Component::EventMap {
+    class MemoryEvent {
+        public:
+            MemoryEvent() = default;
+            ~MemoryEvent() = default;
+
+            static void SendOptimisedMessage(
+                std::string connectionUUID,
+                Cardinal::Entity::Message& message,
+                Service::MemoryServiceInterface& memoryService,
+                Service::MessageServiceInterface& messageService
+            );
+    };
+}
+#endif
