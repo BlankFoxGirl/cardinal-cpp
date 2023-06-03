f333ba0a6226f02c9a71c9649761b3eba7c97f52
Sarah Jabado <sarmth@icloud.com>
2023-06-03 10:31:08 +0800

    Apply formatting

--- Cardinal/Component/Connection/EndUserClientConnection.hpp
+++ Cardinal/Component/Connection/EndUserClientConnection.hpp
@@ -7,46 +7,46 @@
 #include "Cardinal/Entity/UserEntity.hpp"
 #include <vector>
 
-namespace Cardinal::Component::Connection {
-    class EndUserClientConnection {
-        public:
-            EndUserClientConnection(
-                Cardinal::Service::LogServiceInterface& s,
-                Cardinal::Service::CommunicationServiceInterface& s1,
-                Cardinal::Service::MessageServiceInterface& s2,
-                Cardinal::Service::MemoryServiceInterface& s3
-            ): logService_(s), communicationService_(s1), messageService_(s2), memoryService_(s3)
-            {};
+namespace Cardinal::Component::Connection
+{
+    class EndUserClientConnection
+    {
+    public:
+        EndUserClientConnection(
+            Cardinal::Service::LogServiceInterface &s,
+            Cardinal::Service::CommunicationServiceInterface &s1,
+            Cardinal::Service::MessageServiceInterface &s2,
+            Cardinal::Service::MemoryServiceInterface &s3) : logService_(s), communicationService_(s1), messageService_(s2), memoryService_(s3){};
 
-            ~EndUserClientConnection() = default;
+        ~EndUserClientConnection() = default;
 
-            void AcceptConnection(void *args);
+        void AcceptConnection(void *args);
 
-        private:
-            Cardinal::Service::LogServiceInterface& logService_;
-            Cardinal::Service::CommunicationServiceInterface& communicationService_;
-            Cardinal::Service::MemoryServiceInterface& memoryService_;
-            Cardinal::Service::MessageServiceInterface& messageService_;
+    private:
+        Cardinal::Service::LogServiceInterface &logService_;
+        Cardinal::Service::CommunicationServiceInterface &communicationService_;
+        Cardinal::Service::MemoryServiceInterface &memoryService_;
+        Cardinal::Service::MessageServiceInterface &messageService_;
 
-            static const int POLL_TIMEOUT = 100;
-            static const int MAX_BUFFER_SIZE = 1024;
+        static const int POLL_TIMEOUT = 100;
+        static const int MAX_BUFFER_SIZE = 1024;
 
-            static const int MESSAGE_EXPIRE_TIME = 60;
+        static const int MESSAGE_EXPIRE_TIME = 60;
 
-            std::string ConnectionMemoryKey = "";
+        std::string ConnectionMemoryKey = "";
 
-            Cardinal::Service::req socket;
-            std::vector<char> buffer;
-            Cardinal::Entity::UserEntity* userEntity;
-            int SocketStatus = 1;
-            std::string HandleReadingFromClient(std::vector<char>& buffer);
-            std::string GetMessage();
-            void HandleWritingToClient();
-            bool IsNeedingToSendData();
-            void InboundMessageHandler(std::string message);
-            void EstablishMemoryQueue();
-            void PullMemoryQueue();
-            bool HasItemsInMemoryQueue();
+        Cardinal::Service::req socket;
+        std::vector<char> buffer;
+        Cardinal::Entity::UserEntity *userEntity;
+        int SocketStatus = 1;
+        std::string HandleReadingFromClient(std::vector<char> &buffer);
+        std::string GetMessage();
+        void HandleWritingToClient();
+        bool IsNeedingToSendData();
+        void InboundMessageHandler(std::string message);
+        void EstablishMemoryQueue();
+        void PullMemoryQueue();
+        bool HasItemsInMemoryQueue();
     };
 }
 #endif
